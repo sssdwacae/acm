123443e4f139e570fed8705099cd73aee60edd5a
sssdwacae <3097014662@qq.com>
2024-06-17 23:36:53 +0800

    main

--- contest/The 2024 Sichuan Provincial Collegiate Programming Contest/F.cpp
+++ contest/The 2024 Sichuan Provincial Collegiate Programming Contest/F.cpp
@@ -6,18 +6,14 @@ void solve()
 {
     int x,y,r,vx,vy,x1,y1,x2,y2;
     cin>>x>>y>>r>>vx>>vy>>x1>>y1>>x2>>y2;
-
     //以x,y为原点
     x1=x1-x;x2=x2-x;y1=y1-y;y2=y2-y;
-
     //点在矩形内
     x1+=r;y1+=r;x2-=r;y2-=r;
-
     if(0<=x2&&0<=y2&&0>=x1&&0>=y1){
         cout<<"Yes\n";
         return;
     }
-
     //裁剪矩形
     if(vx>0&&x1<0)
         x1=0;
@@ -27,12 +23,10 @@ void solve()
         y1=0;
     else if(vy<0&&y2>0)
         y2=0;
-    
     if(x1>x2||y1>y2){
         cout<<"No\n";
         return;
     }
-
     //四条坐标轴
     if(vx==0){
         if(( (vy>0&&y2>0)||(vy<0&&y1<0) )&&x1<=0&&x2>=0)
@@ -59,7 +53,6 @@ void solve()
     cout<<"No\n";
     return;
 }
-
 signed main()
 {
     int T;
