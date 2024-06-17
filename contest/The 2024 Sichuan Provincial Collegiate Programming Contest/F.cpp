ea106a048b7f2343512b7c7fa9c4e874323960f4
sssdwacae <3097014662@qq.com>
2024-06-17 12:19:53 +0800

    main

--- contest/The 2024 Sichuan Provincial Collegiate Programming Contest/F.cpp
+++ contest/The 2024 Sichuan Provincial Collegiate Programming Contest/F.cpp
@@ -7,16 +7,38 @@ void solve()
     int x,y,r,vx,vy,x1,y1,x2,y2;
     cin>>x>>y>>r>>vx>>vy>>x1>>y1>>x2>>y2;
     x1-=x;x2-=x;y1-=y;y2-=y;
-    if(x!=0){
-        double left=max((double)(x1+r),1.0*(y1+r)*vx/vy);
-        double right=min((double)x2-r,1.0*(y2-r)*vx/vy);
-        if(left<=right)
-            cout<<"Yes\n";
-        else
-            cout<<"No\n";
+    if(vx!=0){
+        if(vy!=0){
+            double left,right;
+            if(vx*vy>0){
+                left=max((double)(x1+r),1.0*(y1+r)*vx/vy);
+                right=min((double)x2-r,1.0*(y2-r)*vx/vy);
+            }
+            else{
+                left=max((double)(x1+r),1.0*(y2-r)*vx/vy);
+                right=min((double)x2-r,1.0*(y1+r)*vx/vy);
+            }
+            if(vx>0)
+                left=0;
+            else
+                right=0;
+            //cout<<left<<' '<<right<<endl;
+            if(left<=right)
+                cout<<"Yes\n";
+            else
+                cout<<"No\n";
+        }
+        else{
+            if((vx>0&&(x2-max(x1,0)>=2*r)||(vx<0&&(min(x2,0)-x1>=2*r)))&&y1<=-r&&y2>=r)
+                cout<<"Yes\n";
+            else
+                cout<<"No\n";
+        }
+        
+
     }
     else{
-        if(y2-y1>=2*r&&x1<=-r&&x2>=r)
+        if((vy>0&&(y2-max(y1,0)>=2*r)||(vy<0&&(min(y2,0)-y1>=2*r)))&&x1<=-r&&x2>=r)
             cout<<"Yes\n";
         else
             cout<<"No\n";
