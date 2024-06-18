a25f7acdbee46461a69d1915f3270f18bebff45a
sssdwacae <3097014662@qq.com>
2024-06-18 23:05:40 +0800

    main

--- test/CPH.cpp
+++ test/CPH.cpp
@@ -1,85 +1,255 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-using pii = pair<int, int>;
-#define endl '\n'
-
-int t;
-
-double x, y, r, vx, vy, lx, ly, rx, ry;
-double x1, x2, Y1, y2, midx, midy, k;
-
-void solve()
-{
-    cin >> x >> y >> r >> vx >> vy >> lx >> ly >> rx >> ry;
-
-    if (ry - ly < 2 * r || rx - lx < 2 * r)
-    {
-        cout << "No\n";
-        return;
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+ 
+#define random(x) (rand() % x)
+#define MULTIPLE 3
+ 
+typedef struct page_s {
+    int n;                      //number
+    int v;                      //visit flag
+}page;
+ 
+char *menu[] = {
+    "o - OPT",
+    "f - FIFO",
+    "r - LRU",
+    "q - quit",
+    NULL
+};
+ 
+int getchoice(char *greet, char *choices[]) {
+    int chosen = 0;
+    int selected;
+    char **option;
+ 
+    do {
+        printf("Choice: %s\n", greet);
+        option = choices;
+        while(*option) {
+            printf("%s\n", *option);
+            option++;
+        }
+        do {
+            selected = getchar();
+        } while(selected == '\n');
+        option = choices;
+        while(*option) {
+            if(selected == *option[0]) {
+                chosen = 1;
+                break;
+            }
+            option++;
+        }
+        if(!chosen) {
+            printf("Incorrect choice, select again\n");
+        }
+    } while(!chosen);
+    return selected;    
+}
+void buildPageReference(int size, page **reference, page *program) {
+    int i;
+    int n;
+    printf("Page reference : ");
+    for(i=0;i<size;i++) {
+        n = random(size/MULTIPLE);
+        reference[i] = &program[n];
+        program[n].n = n;
+        program[n].v = 0;
+        printf("| %d ", n);
     }
-
-    if (vx == 0)
-    {
-        x1 = x2 = x;
-        // Y1 = ly, y2 = ry;
-        if ((ly - y) * vy <= 0)
-            Y1 = y;
-        else
-            Y1 = ly;
-        if ((ry - y) * vy <= 0)
-            y2 = y;
-        else
-            y2 = ry;
+    printf("\n");
+}
+void print(int n, page *frame, int size) {
+    int i;
+ 
+    printf("no. %d step: ", n);
+    for(i=0;i<size;i++) {
+        printf("| %d ", frame[i].n);
     }
-    else if (vy == 0)
-    {
-        Y1 = y2 = y;
-        // x1 = lx, x2 = rx;
-        if ((lx - x) * vx <= 0)
-            x1 = x;
-        else
-            x1 = lx;
-        if ((rx - x) * vx <= 0)
-            x2 = x;
-        else
-            x2 = rx;
+    printf("\n");
+}
+int Search(int n, page *list, int size) {
+    int i;
+    for(i=0;i<size;i++) {
+        if(list[i].n == n) return i;
     }
-    else
-    {
-        if ((lx - x) * vx <= 0)
-            x1 = x;
-        else
-            x1 = lx;
-        if ((rx - x) * vx <= 0)
-            x2 = x;
-        else
-            x2 = rx;
-
-        Y1 = (double)(x1 - x) * vy / vx + y;
-        y2 = (double)(x2 - x) * vy / vx + y;
+    return -1;
+}
+ 
+int findNext(int n, page **list, int start, int size) {
+    int count = size;
+    int i;
+    for(i=start;i<size;i++) {
+        if(list[i]->n == n) break;
+        else count++;
     }
-
-    // cout << x1 << ' ' << Y1 << ' ' << x2 << ' ' << y2 << '\n';
-
-    if (x1 > x2)
-        swap(x1, x2);
-    if (Y1 > y2)
-        swap(Y1, y2);
-    if (x1 + r <= rx && x2 - r >= lx && Y1 + r <= ry && y2 - r >= ly)
-        cout << "Yes\n";
-    else
-        cout << "No\n";
+    return count;
+}
+ 
+int findLastMax(page *frame, int size) {
+    int tmp=0,s,i,j=0;
+    for(i=0;i<size;i++) {
+        s = frame[i].v;
+        if(s > tmp) {
+            tmp = s;
+            j = i;
+        }        
+    }
+    return j;
+}
+ 
+ 
+int findLastMin(page *frame, int size) {
+    int tmp=frame[0].v,s,i,j=0;
+    //printf("| %d ", tmp);
+    for(i=1;i<size;i++) {
+        s = frame[i].v;
+       // printf("| %d ", s);
+        if(s < tmp) {
+            tmp = s;
+            j = i;
+        }
+    }
+   // printf("\n");
+    return j;
+}
+ 
+void fifo(int fsize, page *frame, int rsize, page **pageR) {
+    int i, j=0,p=0;
+    int f=0;
+    for(i=0;i<fsize;i++) frame[i].n = -1;
+    for(i=0;i<rsize;i++) {
+        if(Search(pageR[i]->n, frame, fsize)!=-1);
+        else if(i<fsize || p<fsize) {
+            frame[p].n=pageR[i]->n;
+            p++;
+        }
+        else {
+            frame[j%fsize].n = pageR[i]->n;
+            j++;
+            f++;
+        }
+        print(i, frame, fsize);
+    }
+    printf("page fault : %d\n", f);
+    
+}
+ 
+void lru(int fsize, page *frame, int rsize, page **pageR) {
+    int i, j, p=0, q;
+    int f=0;
+    for(i=0;i<fsize;i++) {
+        frame[i].n = -1;
+        frame[i].v = 0;
+    }
+    for(i=0;i<rsize;i++) {
+        for(j=0;j<fsize;j++) {
+            if(frame[j].n!=-1) frame[j].v++;
+        }
+        q = Search(pageR[i]->n, frame, fsize);
+        if(q!=-1) frame[q].v=0;
+        else if(i<fsize || p<fsize) {
+            frame[p].n=pageR[i]->n;
+            p++;
+        }else {
+            q = findLastMax(frame, fsize);
+            frame[q].n = pageR[i]->n;
+            frame[q].v = 0;
+            f++;
+        }
+        print(i, frame, fsize);
+   }
+   printf("page fault : %d\n", f);
 }
 
-int main()
-{
-    ios_base::sync_with_stdio(0);
-    cin.tie(0), cout.tie(0);
-    cin >> t;
-    while (t--){
-        solve();
+void opt(int fsize, page *frame, int rsize, page **pageR) {
+    int i, j, k, p=0, q, max;
+    int f=0;
+    int *next;
+    next = (int *)malloc(sizeof(int)*fsize);
+    for(i=0;i<fsize;i++) {
+        frame[i].n = -1;
+        frame[i].v = 0;
     }
-    return 0;
+    for(i=0;i<rsize;i++) {
+        for(j=0;j<fsize;j++) {
+            if(frame[j].n!=-1) frame[j].v++;
+        }
+        q = Search(pageR[i]->n, frame, fsize);
+        if(q!=-1) frame[q].v=0;
+        else if(i<fsize || p<fsize) {
+            frame[p].n=pageR[i]->n;
+            p++;
+        }else {
+            for(j=0;j<fsize;j++) {
+                int found = 0;
+                for(k=i+1;k<rsize;k++) {
+                    if(pageR[k]->n == frame[j].n) {
+                        next[j] = k;
+                        found = 1;
+                        break;
+                    }
+                }
+                if(!found) {
+                    next[j] = rsize;
+                }
+            }
+            max = -1;
+            for(j=0;j<fsize;j++) {
+                if(next[j] > max) {
+                    max = next[j];
+                    q = j;
+                }
+            }
+            frame[q].n = pageR[i]->n;
+            frame[q].v = 0;
+            f++;
+        }
+        print(i, frame, fsize);
+   }
+   printf("page fault : %d\n", f);
 }
 
+
+int main() {
+    int choice = 0;
+    int logSize;
+    int phySize;
+    page *program;
+    page **pageR;
+    page *frame;
+    int prSize;
+ 
+    srand((int)time(0));
+    printf("Enter number of pages in program: ");
+    scanf("%d", &logSize);
+ 
+    printf("Enter number of frames in physical memory: ");
+    scanf("%d", &phySize);
+ 
+    program = (page *)malloc(sizeof(int)*2*logSize);
+    frame = (page *)malloc(sizeof(int)*2*phySize);
+ 
+    prSize = logSize * MULTIPLE;
+    pageR = (page **)malloc(sizeof(int*)*prSize);
+    buildPageReference(prSize, pageR,program);
+    
+    do {
+        choice = getchoice("Please select an action", menu);
+        printf("You have chosen: %c\n", choice);
+        switch(choice) {
+        case 'o' :
+            opt(phySize,frame,prSize,pageR);
+            break;
+        case 'f' :
+            fifo(phySize, frame, prSize, pageR);
+            break;
+        case 'r' :
+            lru(phySize, frame, prSize, pageR);
+            break;
+        default: break;
+        }
+    }while(choice != 'q');
+    exit(0);
+}
