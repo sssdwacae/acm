f5344121984bfb1fac0aab4f8802cf7f9a1f3607
sssdwacae <3097014662@qq.com>
2024-06-22 13:26:38 +0800

    main

--- test/CPH.cpp
+++ test/CPH.cpp
@@ -1,26 +1,30 @@
-#include<bits/stdc++.h>
+//删除大于平均值的数
+#include<iostream>
+#include<iomanip>
 using namespace std;
-
-#define endl '\n'
-
-const int N=1e5+5;
-#define ll long long
-#define mp make_pair
-
-
-void solve()
-{
-
-    return;
-}
-
-signed main()
-{
-    ios::sync_with_stdio(0);
-    cin.tie(0);cout.tie(0);
-    int T;
-    cin>>T;
-    while(T--)
-        solve();     
-    return 0;
+int main()
+{  int a[30],i,j,n;  
+   double avg,s=0.0;
+   cout<<"请输入欲输入数的个数:";     
+cin>>n;
+   cout<<"请输入"<<n<<"个数:";
+   for(i=0;i<n;i++) 
+	   cin>>a[i]; 
+/**************************/
+   for(i=0;i<n;i++)
+	   s=s+a[i];
+   avg=s/n;
+   j=0;
+   for(i=0;i<n;i++)
+   {
+	   
+	   if(a[i]<=avg)
+	   {
+		   a[j]=a[i];
+		   j++;
+	   }
+   }
+   for(i=0;i<j;i++)
+	   cout<< " 低于平均值"<<avg<<"的有："<<a[i]<<endl;
+/**************************/
 }
