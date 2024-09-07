d93ff697181104d11bcc53d3303218ec74939466
sssdwacae <3097014662@qq.com>
2024-09-07 20:26:41 +0800

    main

--- test/CPH.cpp
+++ test/CPH.cpp
@@ -1,36 +1,65 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-vector<int> p;
-bool vis[1005];
-int dp[205][55][1005];
-int main()
+
+#define endl '\n'
+
+const int N=1e5+5;
+#define int long long
+#define mp make_pair
+#define L a[i].l
+#define R a[i].r
+#define POS a[i].pos
+#define B a[i].b
+
+struct node{
+	int l,r,pos,b;
+	node(){}
+	node(int _l,int _r,int _pos,int _b){
+		l=_l;
+		r=_r;
+		pos=_pos;
+		b=_b;
+	}
+};
+
+bool cmp1(node t1,node t2)
 {
-    p.push_back(-1);
-    for (int i = 2; i <= 1000; i++)
-    {
-        if (!vis[i])
-        {
-            p.push_back(i);
-            for (int j = i; j <= 1000; j += i)
-                vis[j] = 1;
-        }
-    }
-    int n, m;
-    scanf("%d%d", &n, &m);
-    dp[0][0][0] = 1;
-    for (int i = 1; i < p.size(); i++)
-    {
-        for (int j = 0; j <= m; j++)
-        {
-            for (int k = 0; k <= n; k++)
-            {
-                if (j && k >= p[i])
-                    dp[i][j][k] = dp[i - 1][j][k] + dp[i][j - 1][k - p[i]];
-                else
-                    dp[i][j][k] = dp[i - 1][j][k];
-            }
-        }
-    }
-    printf("%d\n", dp[p.size() - 1][m][n]);
-    return 0;
+	if(t1.l==t2.l)
+		return t1.b<t2.b;
+	else
+		return t1.l<t1.l;
+}
+
+bool cmp2(node t1,node t2)
+{
+	return t1.pos<t2.pos;
+}
+
+void solve()
+{
+	int n;
+	cin>>n;
+	vector<node> a(n+1);
+	for(int i=1;i<=n;i++){
+		cin>>L>>R;
+		POS=i;
+	}
+	sort(a.begin()+1,a.end(),cmp1);
+	int enda=n;//当前不能跨过的最近区间
+	int 
+	for(int i=n;i>=1;i--){
+
+	}
+	return;
+}
+
+signed main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);cout.tie(0);
+	int T;
+	cin>>T;
+	while(T--)
+		solve();     
+	return 0;
 }
